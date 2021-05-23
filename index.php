fcfc5dc28fbce9aff953e440362868c16c6e48c1
Juan Miguel Muñoz <juan13miguel@gmail.com>
2021-05-23 14:38:07 -0500

    limpieza del archivo

--- index.php
+++ index.php
@@ -2,6 +2,4 @@
 //el siguiente codigo direcicona a l login del usuario
 header("status: 301 moved temporarily");
 header("location: sitioweb/login/login");
-echo "prueba";
-echo"holaaaaaa Miguel Angel " ;
 ?>
